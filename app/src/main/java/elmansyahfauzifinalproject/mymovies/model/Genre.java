3284509f670a92d8b3389b3578070f22f1bf2fb9
Elmansyah <elmansyah09@gmail.com>
2017-08-30 05:53:12 +0700

    Design Detail

---
+++ app/src/main/java/elmansyahfauzifinalproject/mymovies/model/Genre.java
@@ -0,0 +1,8 @@
+package elmansyahfauzifinalproject.mymovies.model;
+
+/**
+ * Created by ALLIANCES on 8/28/2017.
+ */
+
+public class Genre {
+}
---
+++ app/src/main/java/elmansyahfauzifinalproject/mymovies/utils/GenreObject.java
@@ -0,0 +1,8 @@
+package elmansyahfauzifinalproject.mymovies.utils;
+
+/**
+ * Created by ALLIANCES on 8/28/2017.
+ */
+
+public class GenreObjecy {
+}
